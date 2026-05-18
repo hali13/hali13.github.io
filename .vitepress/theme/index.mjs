b6050587894c37a1a884762897efe7fab1d131c8
wcy-akioi <wangcanyuan2012@163.com>
2026-05-18 22:12:05 +0800

    修复

--- .vitepress/theme/index.mjs
+++ .vitepress/theme/index.mjs
@@ -4,6 +4,7 @@ import { routeChange } from "@/utils/initTools.mjs";
 import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
 import LazyLoader from "@/components/LazyLoader.vue";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import Layout from './Layout.vue'
 
 // 根组件
 import App from "@/App.vue";
