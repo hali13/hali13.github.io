74d9932452cb339c65d7e2f4c4b31834c9626519
wcy-akioi <wangcanyuan2012@163.com>
2026-05-18 21:53:29 +0800

    feat: 初始化 Curve 主题博客

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -23,6 +23,7 @@ const themeConfig = await getThemeConfig();
 // https://vitepress.dev/reference/site-config
 export default withPwa(
   defineConfig({
+    base: '/',
     title: themeConfig.siteMeta.title,
     description: themeConfig.siteMeta.description,
     lang: themeConfig.siteMeta.lang,
